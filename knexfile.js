6fb9895511b584451ad8aa334b3f8f85b85f83e2
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-03-27 18:07:43 -0700

    load dotenv; connect to elephantsql db

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,9 @@
-// Update with your config settings.
+require("dotenv").config();
+const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: {
-      filename: "",
-    },
+    connection: DATABASE_URL,
   },
 };
